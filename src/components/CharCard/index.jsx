ab6fd115a915e518a9e98acc92b48fa11ddbd380
Arthur Sarmento <arthurbs17@gmail.com>
2021-09-13 19:05:17 -0300

    Status att

--- src/components/CharCard/index.jsx
+++ src/components/CharCard/index.jsx
@@ -1,7 +1,12 @@
 import "./index.css";
-const CharCard = ({ char: { name, image } }) => {
+const CharCard = ({ char: { name, image, status } }) => {
+  let className = "card";
+  // eslint-disable-next-line no-lone-blocks
+  {
+    status === "Dead" ? (className += " Dead") : (className = "card");
+  }
   return (
-    <div className="card">
+    <div className={className}>
       <h3>{name}</h3>
       <img src={image} alt={name} />
     </div>
